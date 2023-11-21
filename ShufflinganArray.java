5389aea1cf9215d34a13f1a1066e160cdb4d467a
Abel Priyan <101852608+Abel1512@users.noreply.github.com>
2023-11-21 13:15:57 +0530

    Add files via upload

---
+++ ShufflinganArray.java
@@ -0,0 +1,20 @@
+package com.abel;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShufflinganArray {
+    public static void main(String[] args) {
+
+        Integer[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+
+        List<Integer> intList = Arrays.asList(intArray);
+
+        Collections.shuffle(intList);
+
+        intList.toArray(intArray);
+
+        System.out.println(Arrays.toString(intArray));
+    }
+}
